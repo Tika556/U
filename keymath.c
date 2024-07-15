8ea8461e97942a5a666f5a9b420625075be844bc
Tika556 <tika99491@gmail.com>
2024-07-15 15:38:29 +0530

    Update keymath.c

--- keymath.c
+++ keymath.c
@@ -119,6 +119,7 @@ int main(int argc, char **argv)  {
 		
 		break;
 		case '-':
+			for (int i = 1; i <= 10; i++) {
 			if(FLAG_NUMBER)	{
 				Scalar_Multiplication(G,&B,number);
 			}
@@ -126,6 +127,7 @@ int main(int argc, char **argv)  {
 			mpz_set(B.x,C.x);
 			mpz_set(B.y,C.y);
 			Point_Addition(&A,&B,&C);
+			}
 		break;
 		case '/':
 			if(!FLAG_NUMBER)	{
